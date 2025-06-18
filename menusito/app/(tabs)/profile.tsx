2ae5a98b16bf242adc4ecece1481cad146395a61
XxLGTOxX <luisgerardoterrazasobregon@gmail.com>
2025-06-17 23:43:56 -0600

    .

--- menusito/app/(tabs)/profile.tsx
+++ menusito/app/(tabs)/profile.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#fff' }}>Home Screen</Text>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#fff' }}>Profile Screen</Text>
       <Text>Welcome to the main application view.</Text>
     </View>
   );

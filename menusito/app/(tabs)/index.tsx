2a73c7e678210e4e907855431b19099f82fac948
eduarell-cmd <edu.arellanes9@gmail.com>
2025-06-17 21:40:22 -0600

    .

---
+++ menusito/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function IndexScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ya quedó esta madre ahora sí</Text>
+    </View>
+  );
+}
